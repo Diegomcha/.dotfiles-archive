b297586269588a04a60060ec93ecc0b2fc67fb3b
Diego <38155922+Diegomcha@users.noreply.github.com>
2022-03-09 19:22:14 +0100

    Script changes v2

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ ln -s ~/.dotfiles/neofetch.conf ~/.config/fish/config.conf
 # ln -s ~/.dotfiles/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-add-apt-repository ppa:fish-shell/release-3 -y
-add-apt-repository ppa:neovim-ppa/stable -y
-apt update
-apt install fish bat trash-cli neovim neofetch -y
+sudo add-apt-repository ppa:fish-shell/release-3 -y
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install fish bat trash-cli neovim neofetch -y
 
 curl -o exa.deb http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb 
-apt install ./exa.deb -y
+sudo apt install ./exa.deb -y
 rm exa.deb
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt update
-apt install gh
+sudo pt update
+sudo pt install gh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install --lts
