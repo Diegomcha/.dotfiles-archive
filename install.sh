7ed1abc30684ee62329a3602b3b2f7dc71f6225d
Diego <38155922+Diegomcha@users.noreply.github.com>
2022-03-09 19:17:50 +0100

    Script changes

--- install.sh
+++ install.sh
@@ -1,28 +1,39 @@
 cd ~
 
-mkdir -p ~/.config
-mkdir -p ~/.local/share/nvim/site/pack/packer/opt
+mkdir -p ~/.config/fish
+mkdir -p ~/.config/neofetch
+# mkdir -p ~/.local/share/nvim/site/pack/packer/opt
 
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
-ln -s ~/.dotfiles/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
+ln -s ~/.dotfiles/neofetch.conf ~/.config/fish/config.conf
+# ln -s ~/.dotfiles/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 add-apt-repository ppa:fish-shell/release-3 -y
 add-apt-repository ppa:neovim-ppa/stable -y
 apt update
-apt install fish bat thrash-cli nvim -y
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install IlanCosman/tide@v5
+apt install fish bat trash-cli neovim neofetch -y
 
 curl -o exa.deb http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb 
 apt install ./exa.deb -y
 rm exa.deb
 
-nvim -c PackerSync -c q!
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install --lts
 
+fish
+
 npm i -g pnpm
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v5
+
+tide configure
+
+nvim -c PackerSync
